af3f992cd15f44b8997c6347d10d25c1c8661ae4
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-18 22:21:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+def fizzbuzz(int)
+  if int % fizz_3 == 0 and int % fizz_5 == 0
+    return "FizzBuzz"
+  elsif int % fizz_5 == 0
+    return "Buzz"
+  elsif int % fizz_3 == 0
+    return  "Fizz"
+   end
+ end
